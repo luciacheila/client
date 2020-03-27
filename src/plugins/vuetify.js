e0dc7e52ba000256fec7a2fcddb9250ef2dfa672
luciacheila <cheilapedra5504@gmail.com>
2020-03-27 13:23:59 +0000

    ficha7

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,22 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
+import colors from 'vuetify/lib/util/colors';
+
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+    theme: {
+        themes: {
+            light: {
+                primary: "#8E24AA",
+                secondary: "#607D8B",
+                accent: "#4FC3F7",
+                error: "#B71C1C",
+                warning: "#FF9800",
+                info: "#00E5FF",
+                sucess: "#8BC34A",
+            },
+        },
+    },
+})
